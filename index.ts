9b2a0eb345e24063a6ba99b9c60e3484706c2625
nhan6104 <thanhan.nguyen6104@gmail.com>
2025-06-21 23:22:26 +0700

    fix

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user/profile", userProfileRouter);
-app.use("/api/v1/use/feed", postRouter);
+app.use("/api/v1/feed", postRouter);
 
 (async () => {
     try {
